e73175f682b30b8215c4cdc644e0a6ad4ac3fb6f
PhungNhuY <yphung282@gmail.com>
2025-03-04 22:11:46 +0700

    update be deployment

---
+++ mongo-config/mongo-init.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "----------------------------------------"
+echo "------Initializing the replica set------"
+echo "----------------------------------------"
+mongo -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD <<EOF
+rs.initiate({
+  "_id": "rs0",
+  "members": [
+    {
+      "_id": 0,
+      "host": "localhost:27017"
+    }
+  ]
+});
+rs.status();
+EOF
